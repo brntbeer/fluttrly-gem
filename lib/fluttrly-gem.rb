b1a1c47e91d733111abd9c71ef77e5bb9644686b
Brent Beer <brent.beer@gmail.com>
2011-02-08 12:47:33 -0600

    shipit

--- lib/fluttrly-gem.rb
+++ lib/fluttrly-gem.rb
@@ -10,26 +10,36 @@ module Fluttrly
 	# Your code goes here...
 	class Command
 		class << self
-			command = ARGV[0]
-			list = ARGV[1]
-			message ||= ARGV[2]
-			puts command
-			puts list
-			#
-			#	@uri = URI.parse("http://fluttrly.com/#{list}")
-			#
-			#
-			#	#possibly do the actions like @holman does .execute for command?
-			#	# or case statement for quick hackery
-			#	def show(list)
-			#		page.each do |task|
-			#			puts("#{task["task"]["content"]},
-			#					 #{Time.parse(task["task"]["created_at"]).strftime("%m-%d-%Y %H:%M %p")} ") }
-			#		end
-			#	end
-			#
-			def post
+			def execute(*args)
+				command = args.shift
+				list = args.shift
+				message = args.empty? ? nil : args.join(' ')
+
+				parse_arguments(command, list, message)
+
 			end
+			
+			def parse_arguments(command, list, message)
+				return list(list) if command == 'list'
+				return post(list, message) if command == 'post'
+			end
+
+
+			def list(list)
+				uri = URI.parse("http://fluttrly.com/#{list}.json")
+				http = Net::HTTP.new(uri.host, uri.port)
+				response = http.request(Net::HTTP::Get.new(uri.request_uri))
+
+				page = JSON.parse(response.body)
+				page.each do |task|
+					puts("Task => \"#{task["task"]["content"]}\" at => #{Time.parse(task["task"]["created_at"]).strftime("%m-%d-%Y %H:%M %p")} ") 
+				end
+			end
+
+			def post(list=nil, message=nil)
+				puts "bouverdafs"
+			end
+
 		end
 	end
 end
